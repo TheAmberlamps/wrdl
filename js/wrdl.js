f209659d09d46d490a9baea578e3c68176437a54
Paul Unsted <ed__84@hotmail.com>
2022-04-20 18:22:22 -0400

    Worked out a system to populate tiles with letters. They don't clear properly yet and there's something very odd happening with tiles moving when populated, but the basics of the game layout are for the most part complete.

--- js/wrdl.js
+++ js/wrdl.js
@@ -6,13 +6,15 @@ console.log(word);
 
 let guess = [];
 
-let w1 = document.getElementById("g1");
-console.log(w1);
+let grid = document.getElementById("g1").getElementsByClassName("tile");
 
 document.addEventListener("keydown", function (event) {
   if (event.key === "Backspace" && guess.length > 0) {
     console.log("popped");
     guess.splice(-1, 1);
+    for (let i = 0; i < guess.length; i++) {
+      grid[i].innerHTML = guess[i];
+    }
     console.log(guess);
   }
 });
@@ -21,8 +23,12 @@ document.addEventListener("keypress", function (event) {
   if (event.key !== "Enter" && guess.length < 5) {
     console.log("works");
     guess.push(event.key);
+    for (let i = 0; i < guess.length; i++) {
+      grid[i].innerHTML = guess[i];
+    }
   }
   let test = event.key;
   console.log(test);
   console.log(guess);
+  console.log(grid);
 });
