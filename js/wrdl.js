a2df1f9acd88b7a34cb7854f16fdaef465ea14e5
Paul Unsted <ed__84@hotmail.com>
2022-04-23 18:55:03 -0400

    word validation is coming along nicely, individual letter validation is up next

--- js/wrdl.js
+++ js/wrdl.js
@@ -33,10 +33,30 @@ document.addEventListener("keydown", function (event) {
 document.addEventListener("keypress", function (event) {
   if (event.key !== "Enter" && guess.length < 5) {
     console.log("works");
-    guess.push(event.key);
+    guess.push(event.key.toUpperCase());
     fillClr();
     console.log(guess);
   }
   if (event.key === "Enter") {
+    if (guess.length < 5) {
+      alert("Not enough letters");
+    }
+    if (guess.length === 5) {
+      let dictChk = 0;
+      let gComp = "";
+      for (let i = 0; i < guess.length; i++) {
+        gComp += guess[i];
+        console.log(guess[i]);
+      }
+      for (let i = 0; i < dict.length; i++) {
+        dictChk++;
+        if (gComp === dict[i]) {
+          console.log("do individual letter validation here");
+        }
+      }
+      if (dictChk === dict.length) {
+        alert("Word invalid, not in dictionary");
+      }
+    }
   }
 });
